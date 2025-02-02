67c360c1c2eb5ec1c9417f603eb070a3f8fc2ed0
Irene Volodko <ice7aye@gmail.com>
2025-02-02 16:27:59 +0300

    Create hello-sql-world.sql

---
+++ 7kyu/hello-sql-world.sql
@@ -0,0 +1,7 @@
+-- ❓ DESCRIPTION:
+-- Hello SQL!
+-- Return a table with a single column named Greeting with the phrase 'hello world!'
+-- Please use Data Manipulation Language and not Data Definition Language to solve this Kata
+
+-- ✅ SOLUTION:
+SELECT 'hello world!' AS "Greeting";
